a1c52605bc3ef9172ed0bd0f923bbf32cbd21454
cherrysnow-yjj <1796878896@qq.com>
2021-04-17 23:41:30 +0800

    a simple combination of yolov5 and sort algorithm

---
+++ yolov5_sort/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+ # cap = cv2.VideoCapture('data/images/demo.mp4')
+
+    # while (cap.isOpened()):
+    #     ret, frame = cap.read()
+    #     plot_one_box()
+    #     cv2.imshow('image', frame)
+    #     k = cv2.waitKey(20)
+    #     # q键退出
+    #     if (k & 0xff == ord('q')):
+    #         break
+
+    # cap.release()
