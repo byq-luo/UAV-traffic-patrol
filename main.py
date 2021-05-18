ec22c86e6cbdcf1c86552f44e82f877f88b34023
cherrysnow-yjj <1796878896@qq.com>
2021-05-18 10:39:41 +0800

    main function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import argparse
 import numpy as np
 
+from reverse import check_reverse
 from Cascade_LD import lane_detect
 
 K = 100
@@ -14,9 +15,11 @@ K = 100
 def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     # Plots one bounding box on image img
     tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
+    # color = color or [random.randint(0, 255) for _ in range(3)]
+    color = (255, 205, 205)
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    # print('test')
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
@@ -27,7 +30,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--method', type=int, default=0, help='violation vehicle detection methods')
+    parser.add_argument('--method', type=int, default=2, help='violation vehicle detection methods')
     args = parser.parse_args()
 
     # # run yolov5 algorithm
@@ -42,12 +45,17 @@ if __name__ == '__main__':
     data = np.loadtxt('output/demo.txt', delimiter=',')
 
     # load data by opencv
-    vc = cv2.VideoCapture('data/demo.mp4')
+    cap = cv2.VideoCapture('data/demo.mp4')
 
     # get the frame width and height
-    frame_width = vc.get(cv2.CAP_PROP_FRAME_WIDTH)
-    frame_height = vc.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    # print(frame_width)
+    frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    size = (int(frame_width), int(frame_height))
+    out = cv2.VideoWriter('data/outVideo.mp4', fourcc, fps, size)
 
     pos = 0
     frame_id = 2
@@ -57,56 +65,64 @@ if __name__ == '__main__':
         track.append([])
     i = 0
 
-    while (vc.isOpened()):
-        ret, frame = vc.read()
-        # print(frame)
+    status = []
+    for i in range(1000):
+        status.append("ok")
+
+    avg_v_list = {}
+    idx = 0
+
+    print('---------------begin---------------')
+
+    while (cap.isOpened()):
+        print('frame %d' % idx)
+
+        ret, frame = cap.read()
         if frame is None:
             break
 
-        # method 0: 车辆压线检测算法
+        # method 0: 车辆压线检测算法（加速优化版：每30帧执行判断）
         if args.method == 0:
             frame = cv2.resize(frame, (640, 360))
             frame_tensor = torch.FloatTensor(frame)
             frame_tensor = frame_tensor.permute((2, 0, 1))
-            # print(frame_tensor.shape)
-            # print(frame_tensor.type())
-            lane_map = lane_detect(frame_tensor)
-            # print(lane_map.shape)
-            while (data[pos][0] == frame_id):
+            _, lane_map = lane_detect(frame_tensor)
+
+            while (pos < data.shape[0] and data[pos][0] == frame_id):
                 x1 = int((data[pos][2] - 0.5 * data[pos][4]) * 640)
                 y1 = int((data[pos][3] - 0.5 * data[pos][5]) * 320)
                 x2 = int((data[pos][2] + 0.5 * data[pos][4]) * 640)
                 y2 = int((data[pos][3] + 0.5 * data[pos][5]) * 320)
                 cnt = 0
-                # print(y2)
-                for xx in range(x1, x2):
-                    for yy in range(y1, y2):
-                        if xx >= 640 or yy >= 360:
-                            continue
-                        if lane_map[yy][xx] == 1:
-                            cnt += 1
-                if cnt > 5:
-                    plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(data[pos][1]) + ", status:warning")
-                else:
-                    plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(data[pos][1]) + ", status:ok")
+                if frame_id % 30 == 2:
+                    for xx in range(x1, x2):
+                        for yy in range(y1, y2):
+                            if xx >= 640 or yy >= 360:
+                                continue
+                            if lane_map[yy][xx] == 1:
+                                cnt += 1
+                    if cnt > 5:
+                        status[int(data[pos][1])] = "pressing"
+                    else:
+                        status[int(data[pos][1])] = "ok"
+                plot_one_box([x1, y1, x2, y2], frame,
+                             label="id:" + str(int(data[pos][1])) + ", status:" + status[int(data[pos][1])])
                 pos += 1
             frame_id += 1
 
         # method 1: 车辆超速检测算法
         if args.method == 1:
-            while (data[pos][0] == frame_id):
+            while (pos < data.shape[0] and data[pos][0] == frame_id):
                 id = int(data[pos][1] % 1000)
                 v = 0
                 x = data[pos][2]
                 y = data[pos][3]
                 if len(track[id]) > 0:
-                    # print(track[id][len(track[id] - 1)])
                     last_frame = track[id][len(track[id]) - 1][2]
                     if (frame_id - last_frame >= 10):
                         track[id] = []
                 track[id].append((x, y, frame_id))
                 if (len(track[id]) >= 10):
-                    # print(track[id][len(track[id]) - 10])
                     delta_x = x - track[id][len(track[id]) - 10][0]
                     delta_y = y - track[id][len(track[id]) - 10][1]
                     dist = K * math.sqrt(delta_x * delta_x + delta_y * delta_y)
@@ -116,18 +132,59 @@ if __name__ == '__main__':
                     x2 = (data[pos][2] + 0.5 * data[pos][4]) * frame_width
                     y2 = (data[pos][3] + 0.5 * data[pos][5]) * frame_height
                     if v > 50:
-                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(data[pos][1]) + ", v:" +
+                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(int(data[pos][1])) + ", v:" +
                                                                     str('' if v == 0 else round(v,
                                                                                                 2)) + ", status:overspeed")
                     else:
-                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(data[pos][1]) + ", v:" +
+                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(int(data[pos][1])) + ", v:" +
                                                                     str('' if v == 0 else round(v, 2)) + ", status:ok")
                 pos += 1
             frame_id += 1
 
-        cv2.imshow('result', frame)
+        # method 2: 车辆逆行检测算法
+        if args.method == 2:
+            while (pos < data.shape[0] and data[pos][0] == frame_id):
+                id = int(data[pos][1] % 1000)
+                v = 0
+                x = data[pos][2]
+                y = data[pos][3]
+                if len(track[id]) > 0:
+                    last_frame = track[id][len(track[id]) - 1][2]
+                    if (frame_id - last_frame >= 10):
+                        track[id] = []
+                track[id].append((x, y, frame_id))
+                if (len(track[id]) >= 10):
+                    delta_x = x - track[id][len(track[id]) - 10][0]
+                    delta_y = y - track[id][len(track[id]) - 10][1]
+                    dist = K * math.sqrt(delta_x * delta_x + delta_y * delta_y)
+                    v = dist / (10 / 30)
+                    x1 = (data[pos][2] - 0.5 * data[pos][4]) * 640
+                    y1 = (data[pos][3] - 0.5 * data[pos][5]) * 320
+                    x2 = (data[pos][2] + 0.5 * data[pos][4]) * 640
+                    y2 = (data[pos][3] + 0.5 * data[pos][5]) * 320
+                    frame = cv2.resize(frame, (640, 360))
+                    frame_tensor = torch.FloatTensor(frame)
+                    frame_tensor = frame_tensor.permute((2, 0, 1))
+                    a, b = lane_detect(frame_tensor)
+
+                    reverse_flag, avg_v_list = check_reverse([int((x1 + x2) / 2), int((y1 + y2) / 2)], a,
+                                                             np.array([delta_x * 3, delta_y * 3]), avg_v_list)
+                    if reverse_flag is True:
+                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(int(data[pos][1])) + ", status:reverse")
+                    else:
+                        plot_one_box([x1, y1, x2, y2], frame, label="id:" + str(int(data[pos][1])) + ", status:ok")
+                pos += 1
+            frame_id += 1
+
+        cv2.imshow('frame', frame)
+        out.write(frame)  # 写入视频对象
         # q键退出
         if (cv2.waitKey(30) & 0xff == ord('q')):
             break
-    vc.release()
+        idx += 1
+
+    print('---------------end---------------')
+
+    cap.release()
+    out.release()
     cv2.destroyAllWindows()
